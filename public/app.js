88b4c8a6ef336010b3972e7f243159099e460a79
chanokporn <porpai.chanok2537@gmail.com>
2016-03-22 22:18:42 +0700

    showsumbook

--- public/app.js
+++ public/app.js
@@ -3,4 +3,31 @@ angular.module('todoApp', [])
   .controller('TodoListController', function () {
     var todoList = this
     todoList.pai = 'test'
+    todoList.book = []
+    todoList.add = function (id, name, price) {
+      // console.log(id, name, price)
+      var obj = {
+        'id': id,
+        'name': name,
+        'price': price
+      }
+      // todoList.book.push(obj)
+      // console.log(todoList.book)
+      if (todoList.book.length === 0) {
+        obj.sumbook = 1
+        todoList.book.push(obj)
+      } else {
+        var check = false
+        for (var i = 0; i < todoList.book.length; i++) {
+          if (todoList.book[i].id === id) {
+            check = true
+            todoList.book[i].sumbook += 1
+          }
+        }
+      }
+      if (check === false) {
+        obj.sumbook = 1
+        todoList.book.push(obj)
+      }
+    }
   })
