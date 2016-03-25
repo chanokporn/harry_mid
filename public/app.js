402eeae6aad08744df2bf6343cd54a29dabdb55a
chanokporn <porpai.chanok2537@gmail.com>
2016-03-25 19:25:40 +0700

    discount

--- public/app.js
+++ public/app.js
@@ -4,9 +4,9 @@ angular.module('todoApp', [])
     var todoList = this
     todoList.test = 'test'
     todoList.book = []
+    todoList.discountTotal = 0
     todoList.add = function (id, name, price) {
       // console.log(id, name, price)
-      console.log(todoList.book)
       var obj = {
         'id': id,
         'name': name,
@@ -30,5 +30,28 @@ angular.module('todoApp', [])
         obj.sumbook = 1
         todoList.book.push(obj)
       }
+      console.log(todoList.book)
+      var book = todoList.book
+      todoList.discountTotal = todoList.discount(book)
+      console.log(todoList.discountTotal)
+    }
+    var filterData = function (array) {
+      return array.filter((element) => element.sumbook !== 0)
+    }
+    todoList.discount = function (book) {
+      var items = book.map((obj) => {
+        return { sumbook: obj.sumbook, price: obj.price }
+      })
+      var totalDis = 0
+      while (items.length > 1) {
+        var sumprice = items.reduce((sum, item) => sum + item.price, 0)
+        totalDis += ((items.length - 1) / 10) * sumprice
+        items = items.map((obj) => {
+          return { sumbook: obj.sumbook - 1, price: obj.price }
+        })
+        items = filterData(items)
+      }
+      console.log(totalDis)
+      return totalDis
     }
   })
