a40c1a1188bf00133e95069bccb819171148074b
chanokporn <porpai.chanok2537@gmail.com>
2016-03-26 17:27:27 +0700

    tdd

--- test/index.js
+++ test/index.js
@@ -3,7 +3,6 @@ require('mocha-generators').install()
 
 var Nightmare = require('nightmare')
 var chai = require('chai')
-var server = require('./server')
 var should = chai.should()
 
 describe('nightmareJs and mocha and chai', function () {
@@ -12,11 +11,11 @@ describe('nightmareJs and mocha and chai', function () {
   it('ซื้อเล่ม 1 จำนวน 3 เล่ม, ซื้อเล่ม 2 จำนวน 1 เล่ม ส่วนลดต้องเท่ากับ 20', function * () {
     var case1 = yield nightmare
       .goto('http://localhost:5000')
-      .wait(2000)
+
       .click('#b1')
       .click('#b1')
       .click('#b1')
-      .wait(1000)
+
       .click('#b2')
       .evaluate(function () {
         this.price = document.querySelector('#discountTotal').innerHTML
@@ -27,15 +26,15 @@ describe('nightmareJs and mocha and chai', function () {
   it('ซื้อเล่ม 2 จำนวน 3 เล่ม, ซื้อเล่ม 3 จำนวน 3 เล่ม ส่วนลดต้องเท่ากับ 60', function * () {
     var case2 = yield nightmare
       .goto('http://localhost:5000')
-      .wait(2000)
+
       .click('#b2')
       .click('#b2')
       .click('#b2')
-      .wait(1000)
+
       .click('#b3')
       .click('#b3')
       .click('#b3')
-      .wait(1000)
+
       .evaluate(function () {
         this.price = document.querySelector('#discountTotal').innerHTML
         return this.price
@@ -45,17 +44,17 @@ describe('nightmareJs and mocha and chai', function () {
   it('ซื้อเล่ม 3 จำนวน 4 เล่ม, ซื้อเล่ม 4 จำนวน 4 เล่ม, ซื้อเล่ม 5 จำนวน 5 เล่ม  ส่วนลดต้องเท่ากับ 240', function * () {
     var case3 = yield nightmare
       .goto('http://localhost:5000')
-      .wait(2000)
+
       .click('#b3')
       .click('#b3')
       .click('#b3')
       .click('#b3')
-      .wait(1000)
+
       .click('#b4')
       .click('#b4')
       .click('#b4')
       .click('#b4')
-      .wait(1000)
+
       .click('#b5')
       .click('#b5')
       .click('#b5')
@@ -70,18 +69,18 @@ describe('nightmareJs and mocha and chai', function () {
   it('ซื้อเล่ม 1 จำนวน 2 เล่ม, ซื้อเล่ม 3 จำนวน 3 เล่ม, ซื้อเล่ม 6 จำนวน 3 เล่ม, ซื้อเล่ม 7 จำนวน 5 เล่ม ส่วนลดต้องเท่ากับ 300', function * () {
     var case4 = yield nightmare
       .goto('http://localhost:5000')
-      .wait(2000)
+
       .click('#b1')
       .click('#b1')
-      .wait(1000)
+
       .click('#b3')
       .click('#b3')
       .click('#b3')
-      .wait(1000)
+
       .click('#b6')
       .click('#b6')
       .click('#b6')
-      .wait(1000)
+
       .click('#b7')
       .click('#b7')
       .click('#b7')
@@ -96,21 +95,21 @@ describe('nightmareJs and mocha and chai', function () {
   it('ซื้อเล่ม 4 จำนวน 2 เล่ม, ซื้อเล่ม 5 จำนวน 2 เล่ม, ซื้อเล่ม 6 จำนวน 3 เล่ม, ซื้อเล่ม 7 จำนวน 3 เล่ม , ซื้อเล่ม 1 จำนวน 1 เล่ม ส่วนลดต้องเท่ากับ 340', function * () {
     var case4 = yield nightmare
       .goto('http://localhost:5000')
-      .wait(2000)
+
       .click('#b4')
       .click('#b4')
-      .wait(1000)
+
       .click('#b5')
       .click('#b5')
-      .wait(1000)
+
       .click('#b6')
       .click('#b6')
       .click('#b6')
-      .wait(1000)
+
       .click('#b7')
       .click('#b7')
       .click('#b7')
-      .wait(1000)
+
       .click('#b1')
       .evaluate(function () {
         this.price = document.querySelector('#discountTotal').innerHTML
@@ -121,27 +120,27 @@ describe('nightmareJs and mocha and chai', function () {
   it('ซื้อเล่ม 7 จำนวน 3 เล่ม, ซื้อเล่ม 5 จำนวน 2 เล่ม, ซื้อเล่ม 1 จำนวน 4 เล่ม, ซื้อเล่ม 4 จำนวน 1 เล่ม , ซื้อเล่ม 3 จำนวน 5 เล่ม , ซื้อเล่ม 2 จำนวน 1 เล่ม ส่วนลดต้องเท่ากับ 500', function * () {
     var case4 = yield nightmare
       .goto('http://localhost:5000')
-      .wait(2000)
+
       .click('#b7')
       .click('#b7')
       .click('#b7')
-      .wait(1000)
+
       .click('#b5')
       .click('#b5')
-      .wait(1000)
+
       .click('#b1')
       .click('#b1')
       .click('#b1')
       .click('#b1')
-      .wait(1000)
+
       .click('#b4')
-      .wait(1000)
+
       .click('#b3')
       .click('#b3')
       .click('#b3')
       .click('#b3')
       .click('#b3')
-      .wait(1000)
+
       .click('#b2')
       .evaluate(function () {
         this.price = document.querySelector('#discountTotal').innerHTML
@@ -152,29 +151,29 @@ describe('nightmareJs and mocha and chai', function () {
   it('ซื้อเล่ม 6 จำนวน 5 เล่ม, ซื้อเล่ม 7 จำนวน 1 เล่ม, ซื้อเล่ม 4 จำนวน 4 เล่ม, ซื้อเล่ม 3 จำนวน 3 เล่ม , ซื้อเล่ม 2 จำนวน 2 เล่ม , ซื้อเล่ม 1 จำนวน 1 เล่ม , ซื้อเล่ม 5 จำนวน 2 เล่ม ส่วนลดต้องเท่ากับ 700', function * () {
     var case4 = yield nightmare
       .goto('http://localhost:5000')
-      .wait(2000)
+
       .click('#b6')
       .click('#b6')
       .click('#b6')
       .click('#b6')
       .click('#b6')
-      .wait(1000)
+
       .click('#b7')
-      .wait(1000)
+
       .click('#b4')
       .click('#b4')
       .click('#b4')
       .click('#b4')
-      .wait(1000)
+
       .click('#b3')
       .click('#b3')
       .click('#b3')
-      .wait(1000)
+
       .click('#b2')
       .click('#b2')
-      .wait(1000)
+
       .click('#b1')
-      .wait(1000)
+
       .click('#b5')
       .click('#b5')
       .evaluate(function () {
@@ -184,3 +183,5 @@ describe('nightmareJs and mocha and chai', function () {
     case4.should.equal('700')
   })
 })
+
+should
